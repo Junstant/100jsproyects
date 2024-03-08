da14ebab4f8e4370f98eb7c1f28ac7ed18dfad19
Lucas Jung <luquitasemanuel@outlook.com>
2024-03-08 16:52:27 -0300

    Add consts

--- Dia 5 Pomodoro/main.js
+++ Dia 5 Pomodoro/main.js
@@ -1,2 +1,6 @@
 // call the elements in the dom
 const timeCounter = document.querySelector(".timeCounter");
+const startButton = document.querySelector(".startButton");
+const pauseButton = document.querySelector(".pauseButton");
+const breakButton = document.querySelector(".breakButton");
+const resetButton = document.querySelector(".resetButton");
