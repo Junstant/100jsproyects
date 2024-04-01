0d4e2253944b4b97de293df1ceb2dd157704bf53
Lucas Jung <luquitasemanuel@outlook.com>
2024-03-31 21:39:04 -0300

    make all the features work, add custom cursor, responsive, etc

--- Day 9 Daily Quote/main.js
+++ Day 9 Daily Quote/main.js
@@ -1,3 +1,20 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+const quoteDaily = document.querySelector(".quoteDaily");
+const authorDaily = document.querySelector(".authorDaily");
+
+
+window.addEventListener("mousemove", function(e) {
+	const posX = e.clientX;
+	const posY = e.clientY;
+	cursorDot.style.left = `${posX}px`;
+	cursorDot.style.top = `${posY}px`;
+	cursorOutline.animate({
+		left: `${posX}px`,
+		top: `${posY}px`
+	}, {duration: 500, fill: "forwards"});
+});
+
 const data = null;
 
 const xhr = new XMLHttpRequest();
@@ -9,13 +26,19 @@ xhr.addEventListener('readystatechange', function () {
 		const response = JSON.parse(this.response); //convert to object to acces the indivdiual parts
 		const author = response.author;
 		const quotes = response.text;
-		console.log("Author:", author);
-		console.log("Quote:", quotes);
+		if(author === ""){
+		authorDaily.innerHTML = `Author: Unknow`;
+		}
+		else{
+		authorDaily.innerHTML = `Author: ${author}`;
+		}
+		quoteDaily.innerHTML = `"${quotes}"`;
 	}
 });
-
-xhr.open('GET', 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info');
+// get the url to modify with links
+xhr.open('GET', 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info'); 
+// set the api key
 xhr.setRequestHeader('X-RapidAPI-Key', '685e831e0amsh9ce0a6e1d756b63p172a01jsn36294e64c809');
+// set the host to make the request
 xhr.setRequestHeader('X-RapidAPI-Host', 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com');
-
 xhr.send(data);
